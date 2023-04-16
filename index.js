d0558214767dd1890d640a41db758b307b06c424
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 21:07:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,39 +6,24 @@ const app = express();
 const cors = require("cors");
 const scraper = require("./pageScraper");
 const linksGrabber = require("./linksGrabber");
+const fs = require("fs");
 require("dotenv").config();
 
 app.use(cors());
-
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', "*");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// };
-
-// app.use(allowCrossDomain);
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.send("test");
-})
 
 app.post("/scrape", (req, res) => {
   const baseUrl = req.body.url;
   try {
-    linksGrabber(baseUrl)
-    .then(responses => {
+    linksGrabber(baseUrl).then((responses) => {
       let homePageLinks = [...responses];
-      scraper(homePageLinks).then(() => 
-        res.send(
-          "The result file is being generated, please find it in the server folder..."
-        )
-      );
-    })
+      console.log(responses);
+      scraper(homePageLinks).then(() => {
+        var content = fs.readFileSync("test.txt", "utf8");
+        res.send(content);
+      });
+    });
   } catch (e) {
     res.send(e);
   }
