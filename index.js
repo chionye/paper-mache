7f08c38709c8ff26f9333a18736b60397c7ebd65
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 21:10:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+   res.send("test")
+})
+
 app.post("/scrape", (req, res) => {
   const baseUrl = req.body.url;
   try {
