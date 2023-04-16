ae038f39eefb55af2ea34c26d3b82d77008c6621
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 18:58:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const scraper = require("./pageScraper");
-const linksGrabber = require("./linksGrabber");
 require("dotenv").config();
 
-const corsConf = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}
-
-app.use(cors(corsConf));
+app.use(cors());
 
 app.use(express.json());
 
@@ -23,19 +14,7 @@ app.get("/", (req, res) => {
 })
 
 app.post("/scrape", async (req, res) => {
-  const baseUrl = req.body.url;
-  try {
-    let homePageLinks = [...(await linksGrabber(baseUrl))];
-    scraper(homePageLinks).then(() => {
-      const file = `test.txt`;
-      res.download(file);
-      res.send(
-        "The result file is being generated, please find it in the server folder..."
-      )
-      });
-  } catch (e) {
-    res.send(e);
-  }
+ res.send("test")
 });
 
 app.listen(process.env.PORT || 5000);
