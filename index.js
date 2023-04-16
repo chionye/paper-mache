c88b8db4870df21ca0b9fb53d53af68ac3589418
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 20:49:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const scraper = require("./pageScraper");
 const linksGrabber = require("./linksGrabber");
 require("dotenv").config();
 
-//app.use(cors());
+app.use(cors());
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// };
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
 
 app.use(express.json());
 
