2dbf66b2d37f0ccb46c46d7dcb2988aeb87d412d
michael valentine <37175136+chionye@users.noreply.github.com>
2023-04-16 21:19:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ app.get("/", (req, res) => {
 
 app.post("/scrape", (req, res) => {
   const baseUrl = req.body.url;
-   res.send(baseUrl)
-//   try {
-//     linksGrabber(baseUrl).then((responses) => {
-//       let homePageLinks = [...responses];
-//       console.log(responses);
-//       scraper(homePageLinks).then(() => {
-//         var content = fs.readFileSync("test.txt", "utf8");
-//         res.send(content);
-//       });
-//     });
-//   } catch (e) {
-//     res.send(e);
-//   }
+//    res.send(baseUrl)
+  try {
+    linksGrabber(baseUrl).then((responses) => {
+      let homePageLinks = [...responses];
+      console.log(responses);
+      scraper(homePageLinks).then(() => {
+        var content = fs.readFileSync("test.txt", "utf8");
+        res.send(content);
+      });
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.listen(process.env.PORT || 5000);
